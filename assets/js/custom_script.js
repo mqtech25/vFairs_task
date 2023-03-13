41dc39c77d45eb33804be8e40f3ccc3fd49ff979
mqtech25 <mqtech25@gmail.com>
2023-03-13 09:49:34 +0500

    fixed some bugs in mobile view design

--- assets/js/custom_script.js
+++ assets/js/custom_script.js
@@ -1,9 +1,13 @@
-$("#wizard-pic").on("click",(e)=>{
-        e.preventDefault();
-       let browsImg= $("#wizard-picture").click();
-       browsImg.on("change",(e)=>{
-        $("#wizardPicturePreview").attr("src","https://mqtech25.github.io/portfolio/img/mqtech.png");
-        $("#wizardPicturePreview").css("background-color","var(--primary-color)")
-        $("#wizardPicturePreview").css("border-radius","10px")
-       })
-})
+$("#wizard-pic").on("click", (e) => {
+  e.preventDefault();
+  let browsImg = $("#wizard-picture").click();
+  browsImg.on("change", (e) => {
+    console.log();
+    $("#wizardPicturePreview").attr(
+      "src",
+      URL.createObjectURL(browsImg[0].files[0])
+    );
+    $("#wizardPicturePreview").css("background-color", "var(--primary-color)");
+    $("#wizardPicturePreview").css("border-radius", "10px");
+  });
+});
